d44f54b37b4024a49d9b58d504e15619290d5e30
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-11-30 16:52:56 +0100

    login service created

--- user-interface-design/src/app/submit.service.ts
+++ user-interface-design/src/app/submit.service.ts
@@ -10,4 +10,8 @@ export class SubmitService {
   submitForm(name: string, password: string, email: string) {
     console.log(name, password, email)
   }
+
+  loginForm(email: string, password: string) {
+    console.log(email, password)
+  }
 }
