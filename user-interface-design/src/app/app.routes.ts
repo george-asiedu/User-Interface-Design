1ccf0c4a6ecc8dd9bd48616c5d85d5e929f24b05
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-11-30 15:21:06 +0100

    register component created and routing configured

--- user-interface-design/src/app/app.routes.ts
+++ user-interface-design/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Register Page',
+        component: RegisterComponent
+    }
+];
