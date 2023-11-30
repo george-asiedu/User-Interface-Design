05a469c6ec920e0377a742adfbe76b7d57ed5c44
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-11-30 16:19:10 +0100

    forms styling applied

--- user-interface-design/src/app/register/register.component.ts
+++ user-interface-design/src/app/register/register.component.ts
@@ -9,31 +9,38 @@ import { Validators } from '@angular/forms';
   template: `
     <div class="grid-container">
       <div class="bg-img"></div>
-      <form [formGroup]="newAccountForm">
-        <h2 class="heading">Create Account</h2>
-        <div class="inputs">
-          <label for="name">Name</label>
-          <input type="text" formControlName="name" id="name" placeholder="John Doe">
-        </div>
 
-        <div class="inputs">
-          <label for="email">Email</label>
-          <input type="email" formControlName="email" id="name" placeholder="johndoe@gmail.com">
-        </div>
+      <div class="form-group">
+        <form [formGroup]="newAccountForm" class="form">
+          <h2 class="heading">Create Account</h2>
 
-        <div class="inputs">
-          <label for="password">Password</label>
-          <input type="password" formControlName="password" id="**********">
-        </div>
+          <div class="inputs">
+            <label for="name">Name</label>
+            <input type="text" formControlName="name" id="name" placeholder="John Doe">
+          </div>
 
-        <button type="submit" [disabled]="!newAccountForm.valid" class="btn-primary">Create Account</button>
+          <div class="inputs">
+            <label for="email">E-mail</label>
+            <input type="email" formControlName="email" id="name" placeholder="johndoe@gmail.com">
+          </div>
 
-        <div class="google">Sign up with Google</div>
-        <p class="text">
-          Already have an account?
-          <a href="">Login</a>
-        </p>
-      </form>
+          <div class="inputs">
+            <label for="password">Password</label>
+            <input type="password" formControlName="password" id="password" placeholder="********">
+          </div>
+
+          <button type="submit" [disabled]="!newAccountForm.valid" class="btn-primary">Create Account</button>
+
+          <div class="google">
+            <img src="assets/Google-logo.png" alt="google-logo">
+            <p class="google-text">Sign up with Google</p>
+          </div>
+          <p class="text">
+            Already have an account?
+            <a href="">Login</a>
+          </p>
+        </form>
+      </div>
     </div>
   `,
   styleUrl: './register.component.css'
