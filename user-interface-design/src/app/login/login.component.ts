85ca6159c87a346af94e94396df3529f6e13530d
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-11-30 16:51:48 +0100

    login component reactive forms created

--- user-interface-design/src/app/login/login.component.ts
+++ user-interface-design/src/app/login/login.component.ts
@@ -10,10 +10,35 @@ import { Validators } from '@angular/forms';
   template: `
     <div class="grid-container">
       <div class="bg-img"></div>
+      <div class="form-group">
+        <form [formGroup]="applyLogin" class="form">
+          <h2 class="heading">Login</h2>
+
+          <div class="inputs">
+            <label for="email">E-mail</label>
+            <input type="email" formControlName="email" id="email" placeholder="email">
+          </div>
+
+          <div class="inputs">
+            <label for="password">Password</label>
+            <input type="password" formControlName="password" id="password" placeholder="*********">
+          </div>
+
+          <button type="submit">Login</button>
+
+          <p class="text">
+            Already have an account? 
+            <a routerLink="">Sign up</a>
+          </p>
+        </form>
+      </div>
     </div>
   `,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  applyLogin = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
 }
