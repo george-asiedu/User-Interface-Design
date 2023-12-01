35c02966a5fc384b391bf803139f102f3ea8a6fc
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-01 11:50:36 +0100

    interest component template updated
    Interests added on the page for users to select

--- user-interface-design/src/app/interest/interest.component.ts
+++ user-interface-design/src/app/interest/interest.component.ts
@@ -10,6 +10,33 @@ import { HeaderComponent } from '../header/header.component';
       <div class="bg-img"></div>
       <div class="interest-container">
         <app-header></app-header>
+        <div class="interest-box">
+          <h2 class="heading">Hey there! What brings you here?</h2>
+          <p class="interest-text">This will help us make great recommendations.</p>
+          <div class="select-box">
+            <div class="select">
+              <img src="assets/add.svg" alt="add-ico">
+              <p>Social interaction</p>
+            </div>
+
+            <div class="select">
+              <img src="assets/add.svg" alt="add-ico">
+              <p>Personal development</p>
+            </div>
+
+            <div class="select">
+              <img src="assets/add.svg" alt="add-ico">
+              <p>Entertainment and fun</p>
+            </div>
+
+            <div class="select">
+              <img src="assets/add.svg" alt="add-ico">
+              <p>Rewards and recognition</p>
+            </div>
+          </div>
+
+          <button type="button" class="btn">Continue</button>
+        </div>
       </div>
     </div>
   `,
