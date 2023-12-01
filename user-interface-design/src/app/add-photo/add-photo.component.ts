6e19a22f8971e9e6b312d1c1c3526eefb29dcd26
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-01 10:25:30 +0100

    header created in the addPhoto component

--- user-interface-design/src/app/add-photo/add-photo.component.ts
+++ user-interface-design/src/app/add-photo/add-photo.component.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   template: `
     <div class="grid-container">
       <div class="bg-img"></div>
+      <div class="photo-container">
+        <div class="header">
+          <div class="back">
+            <img src="assets/Left.svg" alt="back-arrow-image">
+            <p>Back</p>
+          </div>
+          <span>
+            <b>step 2</b> of 4
+          </span>
+          <p class="exit">Exit</p>
+        </div>
+      </div>
     </div>
   `,
   styleUrl: './add-photo.component.css'
