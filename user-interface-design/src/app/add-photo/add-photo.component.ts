7c4aceef28a869e0d1d23dff251afef23be397a3
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-01 10:39:08 +0100

    add-photo box template created

--- user-interface-design/src/app/add-photo/add-photo.component.ts
+++ user-interface-design/src/app/add-photo/add-photo.component.ts
@@ -18,6 +18,16 @@ import { Component } from '@angular/core';
           </span>
           <p class="exit">Exit</p>
         </div>
+
+        <div class="add-image-box">
+          <h2 class="heading">Add Photo</h2>
+          <p class="photo-text">Add a photo so other members know who you are</p>
+          <div class="add-img">
+            <img src="assets/person.svg" alt="person-icon">
+          </div>
+          <button type="button" class="btn-primary">Upload a photo</button>
+          <button type="button" class="skip-btn">Skip</button>
+        </div>
       </div>
     </div>
   `,
