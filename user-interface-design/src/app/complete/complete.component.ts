850eadc0fa0e734db8d86765aa8c2354bee3ac82
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-04 09:56:22 +0100

    complete page sketch design added

--- user-interface-design/src/app/complete/complete.component.ts
+++ user-interface-design/src/app/complete/complete.component.ts
@@ -14,7 +14,30 @@ import { Component } from '@angular/core';
           get excited for the ultimate quiz experience 
           of your life. Let's do this!
         </p>
-
+        <div class="designs">
+          <div class="group-1">
+            <img src="assets/Group.svg" alt="">
+          </div>
+          <div class="group-2">
+            <img src="assets/Group-2.svg" alt="">
+          </div>
+          <div class="group-3">
+            <img src="assets/Group-3.svg" alt="">
+          </div>
+          <div class="group-4">
+            <img src="assets/Group-4.svg" alt="">
+          </div>
+          <div class="group group-5">
+            <img src="assets/Group-5.svg" alt="">
+          </div>
+          <div class="group-6">
+            <img src="assets/Group-6.svg" alt="">
+          </div>
+          <div class="group-7">
+            <img src="assets/Group-7.svg" alt="">
+          </div>
+          
+        </div>
         <button class="btn-primary" type="button">Continue</button>
       </div>
     </div>
