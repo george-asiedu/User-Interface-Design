aa2b8185800b396f8dd09bc69aabc9827a1313da
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-04 11:11:36 +0100

    upload feature added in AddPhotoCOmponent
    Photo upload feature added and routing configured to route to the interest page when an image is successfully uploaded

--- user-interface-design/src/app/add-photo/add-photo.component.ts
+++ user-interface-design/src/app/add-photo/add-photo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-photo',
@@ -14,9 +15,10 @@ import { HeaderComponent } from '../header/header.component';
           <h2 class="heading">Add Photo</h2>
           <p class="photo-text">Add a photo so other members know who you are</p>
           <div class="add-img">
-            <img src="assets/person.svg" alt="person-icon">
+            <img [src]="imageUrl || 'assets/person.svg'" alt="person-icon">
           </div>
-          <button type="button" class="btn">Upload a photo</button>
+          <input type="file" (change)="onFileSelected($event)" style="display: none;" #fileInput />
+          <button type="button" class="btn" (click)="fileInput.click()">{{ buttonText }}</button>
           <button type="button" class="skip-btn">Skip</button>
         </div>
       </div>
@@ -25,5 +27,28 @@ import { HeaderComponent } from '../header/header.component';
   styleUrl: './add-photo.component.css'
 })
 export class AddPhotoComponent {
+  imageUrl: string | null = null;
+  buttonText: string = "Upload a photo";
 
+  constructor(private router: Router) {}
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    this.previewImage(file);
+    this.buttonText = "Continue";
+    this.navigateToInterestPage();
+  }
+
+  previewImage(file: File): void {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.imageUrl = reader.result as string;
+    };
+  }
+
+  private navigateToInterestPage(): void {
+    // Add the route path to navigate to
+    this.router.navigate(['interest']);
+  }
 }
